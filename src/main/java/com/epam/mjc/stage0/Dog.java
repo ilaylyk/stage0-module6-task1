6e3efe85fa552bb6b8759c4b18100fdf8a2eeb34
ilaylyk <40176935+ilaylyk@users.noreply.github.com>
2022-07-30 01:21:01 +0300

    Update Dog.java

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,12 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
 
+  public Dog() {
+        super("brown", 4, true);
+    }
+
+    public String getDescription() {
+        return "This animal is mostly brown. It has 4 paws and a fur.";
+    }
 }
